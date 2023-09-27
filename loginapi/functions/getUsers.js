a3819ddd261e9a017d529a821c4753968ee6a6fa
Jhonny Costa <jhonnycosta@ibm.com>
2023-09-27 14:36:46 -0300

    format trycacth

--- loginapi/functions/getUsers.js
+++ loginapi/functions/getUsers.js
@@ -5,32 +5,29 @@ exports = function (payload) {
   const projection = {
     "user": 1,
   }
-  
+
   const mongodb = context.services.get("mongodb-atlas");
   const mycollection = mongodb.db("LoginDb").collection("Users");
+  try {
+    return mycollection.findOne(body, projection)
+      .then(result => {
+        if (result) {
+          return {
+            "status": true,
+            result
 
- try {
-  return
-  mycollection.findOne(body, projection)
-    .then(result => {
-    
-      if (result) {
-        return {
-          "status": true,
-          result
+          };
 
-        };
-
-      } else {
-        return {
-          "status": false,
-          "message": "User not found",
-          result
+        } else {
+          return {
+            "status": false,
+            "message": "User not found",
+            result
+          }
         }
-      }
+      })
 
-    })
- } catch (error) {
- return { msg: "Erro ao atualizar campo: " + error.message };
+  } catch (error) {
+    return { msg: "Erro ao atualizar campo: " + error.message };
   }
-};
+}
