913066b59611a275ebd774344f3eb1edaca2a7c0
Jhonny Costa <jhonnycosta@ibm.com>
2023-09-27 09:19:14 -0300

    update getUsers

--- loginapp/functions/getUsers.js
+++ loginapp/functions/getUsers.js
@@ -31,18 +31,26 @@
 //     return  "Hello World!";
 // };
 
-exports = function() {
-    // Criando um objeto JavaScript
+// exports = function() {
+//     // Criando um objeto JavaScript
   
-    const valorDecimal = 1.55621;
+//     const valorDecimal = 1.55621;
   
-    const data = {
-      message: valorDecimal
-    };
+//     const data = {
+//       message: valorDecimal
+//     };
   
   
   
-    // Convertendo o objeto em formato JSON
-    const jsonString = JSON.stringify(data);
-    return jsonString;
+//     // Convertendo o objeto em formato JSON
+//     const jsonString = JSON.stringify(data);
+//     return jsonString;
+//   };
+
+
+exports = function (payload) {
+    const body = payload.query;
+    const mongodb = context.services.get("MongoDB");
+    const mycollection = mongodb.db("logindb").collection("users");
+    return mycollection.find().toArray();
   };
